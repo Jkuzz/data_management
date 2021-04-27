c9ecd9b5dd845f1a8b51834b46b91c742a28876f
Jkuzz <68104924+Jkuzz@users.noreply.github.com>
2021-04-27 15:03:47 +0200

    Add files via upload

---
+++ cuber.py
@@ -0,0 +1,101 @@
+import loader
+import json
+from shutil import copyfile
+
+INPUT_FILE = 'loaded_data.json'  # This name is given to the loader module
+TEMPLATE_FILE = 'template.ttl'
+OUTPUT_FILE = 'out.ttl'
+
+
+def prepare_cubes(input_dict):
+    copyfile(TEMPLATE_FILE, OUTPUT_FILE)
+    return make_form_concepts(input_dict)
+
+
+def make_form_concepts(input_dict):
+    """Discovers all forms of healthcare and creates their concepts"""
+    known_forms = []  # First discover all healthcare forms
+    for item in input_dict:
+        for form in item['pocty_dle_formy_pece']:
+            if form in known_forms:
+                continue
+            known_forms.append(form)
+
+    with open(OUTPUT_FILE, 'a+', encoding='utf-8') as out_file:
+        for i, form in enumerate(known_forms):  # Write each form as concept
+            form_string = f'ex:formConcept{i} a skos:Concept ;\n'\
+                        f'\trdfs:label "{form}" ;\n'\
+                        f'\tskos:inScheme ex:FormConceptScheme .\n\n'
+            out_file.write(form_string)
+
+    return known_forms
+
+
+def make_pop_cube(input_dict):
+    """Creates the first data cube from the input dictionary"""
+    with open(OUTPUT_FILE, 'a+', encoding='utf-8') as out_file:
+        for obs_it, pop_observation in enumerate(input_dict):
+            okres = pop_observation['okres']
+            year = pop_observation['rok']
+            inhabitants = pop_observation['stredni_stav_obyvatel']
+            obs_string = f'ex:observation{obs_it} a qb:Observation ;\n' \
+                         '\tqb:dataSet ex:dataCubeInhabitants ;\n' \
+                         f'\tex:refPeriodDim "{year}"^^xsd:date ;\n' \
+                         f'\tex:refAreaDim "{okres}" ;\n' \
+                         f'\tex:inhabitantsMsr "{inhabitants}"^^xsd:integer .\n\n'
+            out_file.write(obs_string)
+
+
+def get_inhabitants_concept(inhabitants):
+    """This shouldn't be hardcoded but it is :)"""
+    if inhabitants < 50000:
+        level = 0
+    elif inhabitants < 100000:
+        level = 1
+    elif inhabitants < 150000:
+        level = 2
+    else:
+        level = 3
+    return f'ex:inhabitantsConcept{level}'
+
+
+def make_hc_cube(input_dict, known_forms):
+    """Creates the second data cube from the input dictionary"""
+    with open(OUTPUT_FILE, 'a+', encoding='utf-8') as out_file:
+        for obs_it, observation in enumerate(input_dict):
+            if observation['pocty_dle_formy_pece'] == {}:
+                continue
+            okres = observation['okres']
+            year = observation['rok']
+            inhabitants = observation['stredni_stav_obyvatel']
+            for hc_form in observation['pocty_dle_formy_pece']:
+                form = f'ex:FormConcept{known_forms.index(hc_form)}'
+                form_providers = observation['pocty_dle_formy_pece'][hc_form]
+                obs_string = f'ex:observation{obs_it} a qb:Observation ;\n' \
+                             '\tqb:dataSet ex:dataCubeHealthcare ;\n' \
+                             f'\tex:refPeriodDim "{year}"^^xsd:date ;\n' \
+                             f'\tex:refAreaDim "{okres}" ;\n' \
+                             f'\tex:inhabitantsDim "{get_inhabitants_concept(inhabitants)}" ;\n' \
+                             f'\tex:healthcareFormDim {form} ;\n' \
+                             f'\tex:providersMsr "{form_providers}"^^xsd:integer .\n\n'
+                out_file.write(obs_string)
+
+
+def make_cubes():
+    """Creates data cubes as per HW2 from data loaded by HW1"""
+    with open(INPUT_FILE, 'r', encoding='utf-8') as in_file:
+        input_dict = json.load(in_file)['data']
+
+    known_forms = prepare_cubes(input_dict)
+    make_pop_cube(input_dict)
+    print('Population data cube created.')
+    make_hc_cube(input_dict, known_forms)
+    print('Healthcare providers data cube created.')
+
+
+if __name__ == '__main__':
+    print('Running cuber pipeline...')
+    loader.load(INPUT_FILE)
+    print('Loading finished.\nCreating data cube...')
+    make_cubes()
+    print('Creation of data cubes finished.')
---
+++ loader.py
@@ -0,0 +1,78 @@
+import csv
+import json
+import glob
+
+# https://data.gov.cz/datov%C3%A1-sada?iri=https%3A%2F%2Fdata.gov.cz%2Fzdroj%2Fdatov%C3%A9-sady%2Fhttps---opendata.mzcr.cz-api-3-action-package_show-id-nrpzs
+HEALTHCARE_FILE = 'narodni-registr-poskytovatelu-zdravotnich-sluzeb.csv'
+
+# https://data.gov.cz/datov%C3%A1-sada?iri=https%3A%2F%2Fdata.gov.cz%2Fzdroj%2Fdatov%C3%A9-sady%2Fhttp---vdb.czso.cz-pll-eweb-package_show-id-130141r20
+# https://data.gov.cz/datov%C3%A1-sada?iri=https%3A%2F%2Fdata.gov.cz%2Fzdroj%2Fdatov%C3%A9-sady%2Fhttp---vdb.czso.cz-pll-eweb-package_show-id-130141r19
+population_files = glob.glob('./130141-??data20??.csv')
+
+# https://data.gov.cz/datov%C3%A1-sada?iri=https%3A%2F%2Fdata.gov.cz%2Fzdroj%2Fdatov%C3%A9-sady%2Fhttp---vdb.czso.cz-pll-eweb-package_show-id-cis101
+OKRES_FILE = 'CIS0101_CS.csv'
+
+OUTPUT_FILE = 'out.json'
+
+
+def load(output_filename):
+    """Reads input data files and creates a single data file."""
+    out_dict = {}
+    okres_dict = {}
+
+    with open(OKRES_FILE, 'r', encoding='cp1250') as okres_file:
+        reader_okres = csv.reader(okres_file)
+        next(reader_okres)  # skip header
+        next(reader_okres)  # skip blank okres
+        for row_okres in reader_okres:
+            if not row_okres:
+                continue
+            okres_nuts = row_okres[8]
+            okres_dict[row_okres[3]] = okres_nuts
+
+    for p_file_name in population_files:
+        year = p_file_name[-8:-4]
+        with open(p_file_name, 'r', encoding='utf-8') as csv_file_pop:
+            reader_pop = csv.reader(csv_file_pop)
+            next(reader_pop)  # skip header
+            for row_pop in reader_pop:
+                if row_pop and row_pop[2] == 'DEM0004':
+                    if row_pop[5] == '101' or row_pop[6] == '554782':
+                        if row_pop[5] == '101':  # kod ciselniku hodnoty
+                            okres_nuts = okres_dict[row_pop[6]]  # find the okres code using CIS0101
+                        elif row_pop[6] == '554782':  # Special case for Praha
+                            okres_nuts = 'CZ0100'
+                        out_dict[(okres_nuts, year)] = {
+                            'okres': okres_nuts,
+                            'rok': year,
+                            'stredni_stav_obyvatel': 0,
+                            'pocty_dle_formy_pece': {}
+                        }
+                        out_dict[(okres_nuts, year)]['stredni_stav_obyvatel'] += int(row_pop[1])
+
+    with open(HEALTHCARE_FILE, 'r', encoding='utf-8') as csv_file_hc:
+        reader_hc = csv.reader(csv_file_hc)
+        next(reader_hc)  # skip header
+        for row_hc in reader_hc:
+            nuts = row_hc[12]
+            if not nuts:  # nejaka pani doktorka Ivanka si neumi vyplnit okres Praha, jen Kraj Praha a pak to pada
+                nuts = row_hc[10] + '0'  # happens once, kraj Praha => okres Praha
+                if nuts not in okres_dict.values():
+                    print('Missing okres NUTS in healthcare provider, skipping.')
+                    print(row_hc)
+                    continue
+            all_forms = row_hc[28].split(', ')
+            for form in all_forms:
+                if not form:
+                    continue
+                all_forms = out_dict[(nuts, '2019')]['pocty_dle_formy_pece']  # 2019 year hardwired!!
+                all_forms[form] = all_forms.get(form, 0) + 1
+
+    with open(output_filename, 'w', encoding='utf-8') as out_file:
+        root_dict = {'data': list(out_dict.values())}
+        json.dump(root_dict, out_file, indent=4, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    print('Running standalone loader')
+    load(OUTPUT_FILE)
